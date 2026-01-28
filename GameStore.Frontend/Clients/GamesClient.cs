0856641258fefb0b863bfb4fde9995865d3b2e17
PhyoMinHein <pmhozzie@gmail.com>
2026-01-29 01:06:30 +0630

    Use component parameters

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -18,33 +18,65 @@ public class GamesClient
 
   public void AddGame(GameDetails newGame)
   {
-    ArgumentException.ThrowIfNullOrWhiteSpace(newGame.GenreId);
+    Genre? genre = GetGenreById(newGame.GenreId);
     var gameSummary = new GameSummary
     {
       Id = games.Count + 1,
       Name = newGame.Name,
-      Genre = genres.FirstOrDefault(g => g.Id == int.Parse(newGame.GenreId))?.Name ?? "Unknown",
+      Genre = genre?.Name ?? "Unknown",
       Price = newGame.Price,
       ReleaseDate = newGame.ReleaseDate
     };
 
-    games.Add(gameSummary);   
+    games.Add(gameSummary);
   }
 
+  public void UpdateGame(GameDetails updatedGame)
+  {
+    var genre = GetGenreById(updatedGame.GenreId);
+    var existingGame = GetGameSummaryById(updatedGame.Id);
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = genre?.Name ?? "Unknown";
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+  }
+
+  
+
   public GameDetails GetGameDetails(int id)
+  {
+    GameSummary gameSummary = GetGameSummaryById(id);
+
+    var genre = genres.FirstOrDefault(d => string.Equals(d.Name, gameSummary.Genre, StringComparison.OrdinalIgnoreCase));
+
+    return new GameDetails
     {
-        GameSummary? gameSummary = games?.FirstOrDefault(g => g.Id == id);
-        ArgumentNullException.ThrowIfNull(gameSummary);
+      Id = gameSummary.Id,
+      Name = gameSummary.Name,
+      GenreId = genre?.Id.ToString(),
+      Price = gameSummary.Price,
+      ReleaseDate = gameSummary.ReleaseDate
+    };
+  }
 
-        var genre = genres.FirstOrDefault(d => string.Equals(d.Name, gameSummary.Genre, StringComparison.OrdinalIgnoreCase));
+  private Genre? GetGenreById(string? id)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(id);
+    var genre = genres.FirstOrDefault(g => g.Id == int.Parse(id));
+    return genre;
+  }
 
-        return new GameDetails
-        {
-            Id = gameSummary.Id,
-            Name = gameSummary.Name,
-            GenreId = genre?.Id.ToString(),
-            Price = gameSummary.Price,
-            ReleaseDate = gameSummary.ReleaseDate
-        };
-    }
+  private GameSummary GetGameSummaryById(int id)
+  {
+    GameSummary? gameSummary = games?.FirstOrDefault(g => g.Id == id);
+    ArgumentNullException.ThrowIfNull(gameSummary);
+    return gameSummary;
+  }
+
+  public void DeleteGame(int id)
+  {
+    var gameSummary = GetGameSummaryById(id);
+    games.Remove(gameSummary);
+  }
 }
